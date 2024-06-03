6a44684cbc253fa7b057691a6a473c923ab0a210
Nasim Alam <nasimal786@gmail.com>
2024-06-03 17:08:21 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const logo = document.getElementById('animated-logo');
 let clickCount = 0;
 
-logo.addEventListener('click', () => {
+document.body.addEventListener('click', () => {
     clickCount++;
 
     switch (clickCount % 3) {
         case 1:
             logo.style.left = '100%';
-            logo.style.transform = 'translate(-100%, -50%) '; 
+            logo.style.transform = 'translate(-100%, -50%)'; 
             break;
         case 2:
-            logo.style.transform = 'translate(-50%, -100%) '; 
+            logo.style.transform = 'translate(-50%, -100%)'; 
             logo.style.left = '50%';
             logo.style.top = '100%';
             break;
